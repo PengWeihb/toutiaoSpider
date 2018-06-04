e910283892ad76f3d55aa3d3f9c3f04a89ea5ca3
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2018-06-04 17:42:36 +0800

    获取头条号主userID
    通过模拟点击的方式，利用headless浏览器PhantomJS以及selenium工具包实现号主抓取

---
+++ userRearch.py
@@ -0,0 +1,98 @@
+# -*- coding:utf-8 -*-
+
+import re
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from MySQLdb import *
+
+def user_rearch(style,user_list_1):
+    url = 'https://www.toutiao.com/ch/' + style + '/'
+    print(url)
+    print type(user_list_1)
+
+    driver = webdriver.PhantomJS()
+    driver.get(url)
+    time.sleep(1)
+    driver.refresh()
+    driver.implicitly_wait(1)
+
+    for i in range(200000):
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        response = soup.find_all('a',{'class':'lbtn source'})
+
+        for links in response:
+            print links
+            links = str(links)
+            pattern = re.compile(r'\d+')
+            body = re.findall(pattern,links)
+
+            if len(body) != 0:
+                data = body[0]
+                data = str(data)
+                data.strip()
+                user_list2 = user_list_1
+                print len(user_list2)
+
+                user_list2.append(data)
+                print len(user_list2)
+                time.sleep(0.5)
+
+                new_user_list = list(set(user_list2))
+                new_user_list.sort(key = user_list2.index)
+                print len(new_user_list)
+
+                if len(new_user_list) == len(user_list_1):
+                    print 'good!'
+                    userId = new_user_list[-1]
+                    userId = str(userId)
+                    MainUrl = 'https://www.toutiao.com/c/user/'+ str(userId) + '/'
+                    mid = 'None'
+                    sql = """insert into Media(MainUrl,userId,mid) value(%s,%s,%s)"""
+                    try:
+                        conn.execute(sql,(MainUrl,userId,mid))
+                        db.commit()
+                    except:
+                        db.rollback()
+                else:
+                    pass
+
+                user_list_1 = new_user_list
+            else:
+                pass
+
+        print str(i)+' is ok'
+        time.sleep(1)
+        if i%100 == 0:
+            time.sleep(1800)
+        else:
+            pass
+        driver.refresh()
+        user_list_1 = new_user_list
+
+
+if __name__ == "__main__":
+    db = connect(host="172.16.0.3", port=3306, db="Spider", user="root", passwd="zy79117911#", charset="utf8")
+    conn = db.cursor()
+    try:
+        sql = 'SELECT userId FROM Media'
+        MainUrl = conn.execute(sql)
+        data = conn.fetchall()
+        db.commit()
+    except:
+        db.rollback()
+    user_list = []
+    for i in range(len(data)):
+        user_id = data[i][0]
+        user_id = str(user_id)
+        user_list.append(user_id)
+    print user_list
+
+    user_set = set(user_list)
+    user_list_1 = list(user_set)
+    user_list_1.sort(key = user_list.index)
+    print len(user_list_1)
+
+    style = 'news_game'
+    user_rearch(style,user_list_1)
+    db.close()
