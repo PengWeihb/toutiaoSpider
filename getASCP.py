2a65694c27c12c40259d50693d2a4eb106e1211d
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2018-06-07 20:19:06 +0800

    获取头条翻页请求需要的参数AS和CP

---
+++ getASCP.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+import hashlib
+import time
+import math
+
+def get_ASCP():
+    t = int(math.floor(time.time()))
+    e = hex(t).upper()[2:]
+    m = hashlib.md5()
+    m.update(str(t).encode(encoding='utf-8'))
+    i = m.hexdigest().upper()
+
+    if len(e) != 8:
+        AS = '479BB4B7254C150'
+        CP = '7E0AC8874BB0985'
+        return AS,CP
+    n = i[0:5]
+    a = i[-5:]
+    s = ''
+    r = ''
+    for o in range(5):
+        s += n[o] + e[o]
+        r += e[o + 3] + a[o]
+
+    AS = 'A1' + s + e[-3:]
+    CP = e[0:3] + r + 'E1'
+    return AS,CP
+
+if __name__ == "__main__":
+    print(get_ASCP())
+    print(get_ASCP()[0])
+    print(get_ASCP()[1])
