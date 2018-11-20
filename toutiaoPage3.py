0a1a49598bf17f7478bed79e196b364e2b639d36
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2018-11-20 10:39:12 +0800

    今日头条文章内容抓取

---
+++ toutiaoPage3.py
@@ -0,0 +1,82 @@
+# -*- coding:utf-8 -*-
+
+from MySQLdb import *
+import time
+import re
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from HTMLParser import HTMLParser
+
+def loadLink(url):
+
+    driver = webdriver.PhantomJS()
+    driver.get(url)
+    time.sleep(0.5)
+    driver.refresh()
+    driver.implicitly_wait(0.5)
+
+    response = BeautifulSoup(driver.page_source, 'lxml')
+
+    try:
+        content = response.find_all('script')
+    except:
+        return '[]'
+
+    if len(content) >= 6:
+        time.sleep(0.02)
+        content = content[6]
+        content = str(content)
+        content = content[28:-12]
+        content = content.strip()
+        content = content.split('},')
+        if len(content) > 2:
+            content = content[2]
+            content = content.strip()
+            content = content.split('content:')
+            if len(content) >= 2:
+                content = content[1]
+                content = content.split('groupId:')
+                content = content[0].strip()
+                content = content[:-1]
+                text = content.replace('div&gt;&lt;', '').replace('&lt;/div&gt;', '')
+                #text = HTMLParser.unescape(text)
+                return text
+            else:
+                return str(content)
+
+        else:
+            return str(content)
+    else:
+        return '[]'
+
+if __name__ == "__main__":
+    db = connect(host="172.16.0.3", port=3306, db="Spider", user="root", passwd="zy79117911#", charset="utf8")
+    conn = db.cursor()
+
+    try:
+        sql = 'SELECT source_url FROM Article5'
+        MainUrl = conn.execute(sql)
+        data = conn.fetchall()
+        db.commit()
+    except:
+        db.rollback()
+    for i in range(2259,len(data)):
+        url = data[i][0]
+        print(url)
+        print(i)
+
+        time.sleep(0.5)
+        page = loadLink(url)
+        n = i + 1
+        n = str(n)
+        params = [page,n]
+        try:
+            sql = """update Article5 set article_content=%s WHERE id=%s"""
+            conn.execute(sql,params)
+            db.commit()
+        except:
+            db.rollback()
+        time.sleep(0.5)
+
+    db.close()
